6a953735ee3fd91c84e6fb2f3713c95ad9816e65
pavliv182 <uptn182@gmail.com>
2022-08-12 12:05:09 +0300

    formEl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import axios from 'axios';
+
+const refs = {
+  formEl: document.querySelector('.search-form'),
+  searchInputEl: document.querySelector('.search-input'),
+  searchBtnEl: document.querySelector('.search-btn'),
+};
